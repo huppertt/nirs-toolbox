aa81310fb3fc02e21bff5d962f3a1799ce118d0f
jeff <jeff@localhost>
2015-02-12 11:15:25 -0500

    Starting to incorporate fNIRS analysis.  Major changes/additions from here on.

---
+++ +nirs/@Data/Data.m
@@ -0,0 +1,68 @@
+classdef Data
+    %DATA Object to hold nirs data
+    
+    properties
+        data;               % channel time series in columns
+        probe;              % object describing geometry
+        time;               % vector of time points
+        Fm = 0;             % modulation frequency in MHz: 0 for CW; 110 for ISS        
+    end
+    
+    properties( Dependent = true )
+        Fs = 0;             % sampling frequency in Hz
+    end
+    
+    properties( Access = private )
+        
+    end
+    
+    methods
+        %% Constructor
+        function obj = Data( data, time, probe, Fm )%, description )
+            if nargin > 0, obj.data = data; end
+            if nargin > 1, obj.time = time; end
+            if nargin > 2, obj.probe = probe; end
+            if nargin > 3, obj.Fm = Fm; end
+        end
+        
+        %% Set/Get
+        function obj = set.Fm( obj, Fm )
+            assert( isscalar(Fm) && obj.Fm >= 0 )
+            obj.Fm = Fm;
+        end
+
+        function obj = set.data( obj, data )
+            assert( ismatrix(data) )
+            obj.data = data;
+        end
+        
+        function obj = set.probe( obj, probe )
+            assert( isobject(probe) || isstruct(probe) )
+            obj.probe = probe;
+        end
+        
+        function obj = set.time( obj, time )
+           assert( isvector(time) )
+           obj.time = time(:);
+        end
+        
+        function out = get.Fs( obj )
+            if length(obj.time) > 1
+                out = 1 / ( obj.time(2) - obj.time(1) );
+            else
+                out = NaN;
+            end
+        end
+        
+        % show data
+        function draw( obj, lstChannels )
+            if nargin == 1
+                plot(obj.data)
+            else
+                plot( obj.data(:,lstChannels) ) 
+            end
+        end
+        
+    end
+end
+
---
+++ +nirs/@Probe/Probe.m
@@ -0,0 +1,168 @@
+classdef Probe
+    %PROBE This object hold nirs probe geometries
+    
+    properties
+        srcPos;     	% nSrc x 3
+        srcDir;      	% nSrc x 3
+        
+        detPos;     	% nDet x 3
+        detDir;       	% nDet x 3
+        
+        % table describing connections with 
+        % source, detector, and type columns        
+        link;           
+        
+        %type = 'wavelength'; % wavelength or chromophore
+    end
+    
+    properties( Dependent = true )
+        distances;
+    end
+    
+    methods
+        %% Constructor
+        function obj = Probe( srcPos, detPos, link )
+            if nargin > 0, obj.srcPos = srcPos; end
+            if nargin > 1, obj.detPos = detPos; end
+            if nargin > 2, obj.link = link; end
+        end
+
+        %% Set/Get
+        % positions
+        function obj = set.srcPos( obj,srcPos )
+            assert( ismatrix(srcPos) )
+            obj.srcPos = srcPos;
+        end
+        
+        function obj = set.detPos( obj,detPos )
+            assert( ismatrix(detPos) )
+            obj.detPos = detPos;
+        end
+        
+        % optional vectors descibing orienatation of src lasers
+        function obj = set.srcDir( obj,srcDir )
+            assert( ismatrix(srcDir) )
+            obj.srcDir = srcDir;
+        end
+        
+        % optional vectors descibing orienatation of det
+        function obj = set.detDir( obj,detDir )
+            assert( ismatrix(detDir) )
+            obj.detDir = detDir;
+        end
+        
+        % a table with source, detector, wavelength/chromophore
+        function obj = set.link( obj,link )
+            assert( istable( link ) && isnumeric(link.source) && isnumeric(link.detector) )
+            obj.link = link;
+        end
+        
+        %% Methods
+        function d = get.distances( obj )
+            isrc = obj.link.Src;
+            idet = obj.link.Det;
+            
+            vec = obj.srcPos(isrc,:) - obj.detPos(idet,:);
+            
+            d = sqrt( sum( vec.^2,2 ) );
+        end
+        
+%         function out = isUniqueSrcs( obj )
+%             % check that sources have unique wavelengths
+%             [~,~,idx1] = unique(obj.link(:,[1 3]),'rows');
+%             [~,~,idx2] = unique(obj.link(:,1));
+%             
+%             if all( idx1 == idx2 )
+%                 out = 1;
+%             else
+%                 out = 0;
+%             end
+%             
+%         end
+%         
+%         function out = isUniqueDets( obj )
+%             % check that detectors have unique wavelengths
+%             [~,~,idx1] = unique(obj.link(:,[2 3]),'rows');
+%             [~,~,idx2] = unique(obj.link(:,2));
+%             
+%             if all( idx1 == idx2 )
+%                 out = 1;
+%             else
+%                 out = 0;
+%             end
+%         end
+%         
+%         function out = isUnique( obj )
+%             out = obj.isUniqueSrcs && obj.isUniqueDets;
+%         end
+%         
+%         function out = isValidSrcs( obj )
+%             out = obj.isUniqueSrcs;
+%             out = out && (max(obj.link(:,1)) == size(obj.srcPos,1));
+%             out = out && (max(obj.link(:,2)) == size(obj.detPos,1));
+%             
+%             out = out && (max(obj.link(:,3)) == length(obj.lambda));
+%         end
+%         
+%         function out = isValidDets( obj )
+%             out = obj.isUniqueDets;
+%             out = out && (max(obj.link(:,1)) == size(obj.srcPos,1));
+%             out = out && (max(obj.link(:,2)) == size(obj.detPos,1));
+%         end
+%         
+%         function out = isValid( obj )
+%             out = obj.isValidSrcs && obj.isValidDets;
+%         end
+        
+        function obj = swapSD( obj )
+            detPos = obj.srcPos;
+            srcPos = obj.detPos;
+            detDir = obj.srcDir;
+            srcDir = obj.detDir;
+            link = obj.link;
+            
+            link.source = obj.link.detector;
+            link.detector = obj.link.source;
+
+            obj.detPos = detPos;
+            obj.srcPos = srcPos;
+            obj.detDir = detDir;
+            obj.srcDir = srcDir;
+            obj.link = link; 
+        end
+        
+        function obj = makeUniqueProbe( obj )
+            % generates an equivalent probe with unique SD idx's
+            [uSrc,~,iSrc] = unique( ...
+                [obj.link.source obj.link.type], ...
+                'rows' );
+            [uDet,~,iDet] = unique( ...
+                [obj.link.detector obj.link.type], ...
+                'rows' );
+            
+            obj.link(:,1:2) = table(iSrc,iDet);
+            
+            obj.srcPos = obj.srcPos( uSrc(:,1),: );
+            obj.detPos = obj.detPos( uDet(:,1),: );
+            
+            if ~isempty(obj.srcDir)
+                obj.srcDir = obj.srcDir( uSrc(:,1),: );
+            end
+            if ~isempty(obj.detDir)
+                obj.detDir = obj.detDir( uDet(:,1),: );
+            end
+        end
+        
+        function draw( obj )
+            if nargin == 1
+                figure, hold on, 
+                plot( obj.detPos(:,1), obj.detPos(:,2), 'bo','MarkerSize',10,'LineWidth',2 )
+                plot( obj.srcPos(:,1), obj.srcPos(:,2), 'rx','MarkerSize',14,'LineWidth',2 )
+                axis normal
+                legend( 'Detectors', 'Sources' )
+            end
+        end
+    
+    end
+end
+
