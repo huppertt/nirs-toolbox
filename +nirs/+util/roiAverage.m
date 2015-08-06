86a119cca44d9310cf5664f82c7f1c3b2d6167bc
jeff <jeff@localhost>
2015-08-06 12:39:49 -0400

    added function to output first level results to table.

--- +nirs/+util/roiAverage.m
+++ +nirs/+util/roiAverage.m
@@ -45,35 +45,6 @@ function tbl = roiAverage( data, R, names )
             tbl = [tbl; tmp];
         end
     end
-
-%     varnames = {'ROI', 'Contrast', 'Beta', 'SE', 'DF', 'T', 'p'};
-%     tbl = table({},[],[],[],[],[],[], 'VariableNames', varnames);
-% 
-%     R = R > 0; 
-% 
-%     for i = 1:size(R,1)
-%         for j = 1:size(data.beta, 1)
-%             roi     = names{i};
-%             con     = data.names{j};
-% 
-%             w       = 1./squeeze( data.covb(j,j,R(i,:)) );
-%             x       = ones(size(w));
-% 
-%             ix      = pinv( x'*diag(w)*x ) * x' * diag(w);
-% 
-%             beta    = ix * data.beta(j,R(i,:))';
-%             se      = sqrt( pinv( x'*diag(w)*x ) );
-% 
-% 
-%             df      = data.dfe;
-%             t       = beta / se;
-%             p       = 2*tcdf(-abs(t),df);
-% 
-%             tmp = cell2table({roi, con, beta, se, df, t, p});
-%             tmp.Properties.VariableNames = varnames;
-%             tbl = [tbl; tmp];
-%         end
-%     end
     
     q   = nirs.math.fdr( tbl.p );
     tbl = [tbl table(q)];
