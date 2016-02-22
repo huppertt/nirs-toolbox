0d7a8b6eb8965b85be3a4f8999665b6605e2a880
huppertt <huppertt@localhost>
2016-02-22 16:57:26 -0500

    Added Monte Carlo to wavelet COH methods

--- +nirs/+math/wavelet_coher.m
+++ +nirs/+math/wavelet_coher.m
@@ -25,7 +25,6 @@ Y=Y-ones(size(Y,1),1)*mean(Y,1);
 Y=Y-ones(size(Y,1),1)*mean(Y,1);
 
 
-
 % TODO: add code to deal with other wavelets, but for now lets use the
 % continuious ones that matlab supports
 scales=[2:256];
@@ -63,7 +62,7 @@ for i=1:size(Y,2)
         
         Z = .5*log((1+WCOH)./(1-WCOH));
         Z=max(min(Z,8),-8);  %avoid having Inf since one value will screw up everything
-        r(i,j)=tanh(median(Z(:)));
+        r(i,j)=tanh(mean(Z(:)));
         r(j,i)=r(i,j);
         cnt=cnt+1;
     end
@@ -71,14 +70,53 @@ end
 disp('completed')
       
 
-% ????? how many DFE are in this model.  Its not the length(a)
-dfe=size(Y,1);
-t=r./sqrt((1-r.^2)/(dfe-2));
-p=2*tcdf(-abs(t),max(dfe));
-p=min(p+eye(size(p)),1);
+dist=montecarlo(50,size(Y),scales,wname,flag_SMOOTH,NSW,NTW);
+Z=.5*log((1+r)./(1-r));
+Z=max(min(Z,8),-8); 
+p=1-dist.cdf(abs(Z));
+
+% 
+% % ????? how many DFE are in this model.  Its not the length(a)
+% dfe=size(Y,1);
+% t=r./sqrt((1-r.^2)/(dfe-2));
+% p=2*tcdf(-abs(t),max(dfe));
+% p=min(p+eye(size(p)),1);
 
 
 end
+
+
+function dist=montecarlo(niter,sizeY,scales,wname,flag_SMOOTH,NSW,NTW);
+
+cnt=1;  Znull=[];
+for iter=1:niter
+    Y=randn(sizeY);
+    for i=1:size(Y,2)
+        cfs_s1(:,:,i)    = cwt(Y(:,i),scales,wname);
+        cfs_s10(:,:,i)   = cfs_s1(:,:,i);
+        cfs_s1(:,:,i)    = smoothCFS(abs(squeeze(cfs_s1(:,:,i))).^2,flag_SMOOTH,NSW,NTW);
+        cfs_s1(:,:,i)    = sqrt(cfs_s1(:,:,i));
+    end
+    for i=1:size(Y,2)
+        for j=i+1:size(Y,2)
+            % -------- Cross wavelet
+            cfs_cross = conj(squeeze(cfs_s10(:,:,i))).*squeeze(cfs_s10(:,:,j));
+            cfs_cross = smoothCFS(cfs_cross,flag_SMOOTH,NSW,NTW);
+            WCOH      = cfs_cross./(squeeze(cfs_s1(:,:,i)).*squeeze(cfs_s1(:,:,j)));
+            
+            Z = .5*log((1+WCOH)./(1-WCOH));
+            Z=max(min(Z,8),-8);  %avoid having Inf since one value will screw up everything
+            Znull(cnt)=mean(Z(:));
+            cnt=cnt+1;
+        end
+    end
+    
+end
+dist=fitdist(Znull','normal');
+
+end
+
+
 
 %----------------------------------------------------------------------
 function CFS = smoothCFS(CFS,flag_SMOOTH,NSW,NTW)
