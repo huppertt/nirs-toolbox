f313703a7dd445cc344ffb01fcdc08aab8d7eda3
Frank Fishburn <fishburnf@upmc.eu>
2017-04-05 12:38:24 -0400

    Provide optional flag in KeepStims to make specific conditions required for subject to be included

--- +nirs/+modules/KeepStims.m
+++ +nirs/+modules/KeepStims.m
@@ -5,6 +5,9 @@ classdef KeepStims < nirs.modules.AbstractModule
     %     listOfStims - cell array of stim names
     properties
         listOfStims = {}; % cell array of stim names
+        required = false; % logical scalar or array [1 x ncond] indicating
+                          % whether stimuli are required (subjects missing them will be removed)
+                          % default: false
     end
     
     methods
@@ -14,7 +17,18 @@ classdef KeepStims < nirs.modules.AbstractModule
         end
         
         function data = runThis( obj, data )
+            
+            obj.required = logical(obj.required);
+            if numel(obj.required)==1
+                obj.required = repmat(obj.required,size(obj.listOfStims));
+            end
+            if length(obj.required) ~= length(obj.listOfStims)
+                error('List of stims and required paramters not compatible. Lengths: %i and %i',length(obj.required),length(obj.listOfStims));
+            end
+            bad_subjects = false(size(data));
+            
             for i = 1:length(data)
+                
                 if(isa(data(i),'nirs.core.Data') | isa(data(i),'eeg.core.Data') | isa(data(i),'dtseries.core.Data'))
                     stim = data(i).stimulus;
                     
@@ -32,16 +46,26 @@ classdef KeepStims < nirs.modules.AbstractModule
                     % new stimulus
                     data(i).stimulus = Dictionary(keys(lst), vals(lst));
                     
+                    if any( obj.required & ~lst )
+                        bad_subjects(i) = true;
+                    end
+                    
                 elseif(isa(data(i),'nirs.core.sFCStats'))
                     cond=data(i).conditions;
                     lst=find(ismember(cond,obj.listOfStims));
                     data(i).R=data(i).R(:,:,lst);
                     data(i).dfe=data(i).dfe(lst);
                     data(i).conditions={data(i).conditions{lst}};
+                    if any(~ismember(obj.listOfStims(obj.required),cond))
+                        bad_subjects(i) = 1;
+                    end
                 else
                     cond=data(i).conditions;
                     cond={cond{ismember(cond,obj.listOfStims)}};
                     data(i)=data(i).ttest(cond);
+                    if any(~ismember(obj.listOfStims(obj.required),cond))
+                        bad_subjects(i) = 1;
+                    end
                 end
             end
             
@@ -49,6 +73,8 @@ classdef KeepStims < nirs.modules.AbstractModule
                 warning('No stim conditions left. Did you provide the correct stim names?')
                 
             end
+            
+            data(bad_subjects) = [];
         end
     end
 end
