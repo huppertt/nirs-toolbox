9eb5cd3d9673ad99d4d9e884f14db5dcd3b821e9
huppertt <huppertt@localhost>
2017-04-27 08:39:39 -0400

    added power analysis

---
+++ +nirs/+math/MDC.m
@@ -0,0 +1,22 @@
+function [mdc,pwr] = MDC(Stats,beta,alpha)
+
+side=1;
+
+if(isa(Stats,'nirs.core.ChannelStats'))
+    
+    t2beta = tinv(beta,Stats.dfe-2);
+    talpha = tinv(1-alpha/side,Stats.dfe-2);
+    
+    mdc = sqrt(diag(Stats.covb))*(t2beta+talpha);
+    
+    %Stats.beta./(sqrt(diag(Stats.covb))=t2beta+talpha;
+    
+    talpha2 = tinv(1-Stats.p/side,Stats.dfe-2);
+    pwr=tcdf(Stats.tstat-talpha2,Stats.dfe-2);
+elseif(isa(Stats,'table'))
+    t2beta = tinv(beta,Stats.DF-2);
+    talpha = tinv(1-alpha/side,Stats.DF-2);
+    mdc = Stats.SE.*(t2beta+talpha);
+    talpha2 = tinv(1-Stats.p/side,Stats.DF-2);
+    pwr=tcdf(Stats.T-talpha2,Stats.DF-2);
+end
