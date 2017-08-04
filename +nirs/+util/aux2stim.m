3235c2a54a6ce25c5c4b4bc8dbdde06187918994
Frank Fishburn <fishburnf@upmc.edu>
2017-08-04 00:13:10 -0400

    Make aux2stim generalize to other systems

--- +nirs/+util/aux2stim.m
+++ +nirs/+util/aux2stim.m
@@ -6,11 +6,12 @@ if(~isfield(data,'aux'))
     return
 end
 
-if(max(data.aux(:))>1500)  %not sure why I did this? (probably specific to a Cw6 system)
+hasmarks=any(bsxfun(@rdivide,bsxfun(@minus,data.aux,median(data.aux)),mad(data.aux))>20);
+if any(hasmarks)
     cnt=1; bin=zeros(length(data.t),0);
     ss=zeros(length(data.t),0);
     for i=1:size(data.aux,2)
-        if(max(data.aux(:,i)>1500))
+        if hasmarks(i)
             slocal=data.aux(:,i);
             slocal=slocal-min(slocal);
             slocal=slocal/max(slocal);           
