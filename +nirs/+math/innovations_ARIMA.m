8bf8613b35458363315df37e8ab66dc3f3a763ae
huppertt <huppertt@localhost>
2017-08-08 15:11:03 -0400

    added ARIMA code (using matlab's built in)

---
+++ +nirs/+math/innovations_ARIMA.m
@@ -0,0 +1,44 @@
+function [yfilt, ARIMA_mdl] = innovations_ARIMA(Y,Pmax,nMA,nI,verbose)
+% This removes auto-correlation and returns the innvations model;
+
+[m, n] = size(Y);
+
+X = []; lst = [];
+
+
+if(nargin<4)
+    nI=0;
+end
+
+if(nargin<5)
+    verbose=false;
+end
+
+if(verbose)
+    h=waitbar(0,'Computing AR-model');
+end
+
+
+% model selection
+for i = 1:n
+    % fit AR model
+    
+    if(verbose)
+        h=waitbar(i/n,h);
+    end
+    
+    y1 = mean(Y(:,i));
+    y = bsxfun(@minus,Y(:,i),y1);
+    
+    mdl=arima(Pmax,nI,nMA);
+    mdl=estimate(mdl,y,'Display','off');
+    yfilt(:,i)=mdl.filter(y);
+    ARIMA_mdl{i}=mdl;
+end
+
+
+if(verbose)
+    try; close(h); end;
+end
+
+return
