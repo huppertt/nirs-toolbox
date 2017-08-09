2c3b4225018c9f3b604b239b0cbd62dc94564a1e
Frank Fishburn <fishburnf@upmc.edu>
2017-08-09 16:11:40 -0400

    Update cross-correlation functions to use full robust algorithm

--- +nirs/+math/robust_xcorrcoef.m
+++ +nirs/+math/robust_xcorrcoef.m
@@ -1,9 +1,8 @@
-function [r,p]=robust_xcorrcoef(d,maxlags,verbose,mask);
-% This is a robust version of the corrcoef function
-% Based on Shevlyakov and Smirnov (2011). Robust Estimation of the
-% Correlation Coeficient: An Attempt of Survey.  Austrian J. of Statistics.
-%  40(1&2) pp. 147-156
+function [r,p] = robust_xcorrcoef( d , maxlags , verbose , mask )
 
+if(nargin<2 || isempty(maxlags))
+    maxlags=size(d,1);
+end
 
 if(nargin<3 || isempty(verbose))
     verbose=false;
@@ -13,10 +12,14 @@ if(nargin<4)
     mask=ones(size(d));
 end
 
+[M,N] = size(d);
+maxlags = ceil(maxlags);
+lags = -maxlags:maxlags;
+numlag = length(lags);
+r = zeros(N,N,numlag);
 
 d=bsxfun(@minus,d,median(d,1));
 d=bsxfun(@rdivide,d,1.4826*mad(d,1,1));
-
 
 W=ones(size(d,1),1);
 for idx=1:size(d,2)
@@ -25,8 +28,45 @@ end
 % Weight the whole model first to down-weight global motion artifacts
 d=spdiags(W,0,length(W),length(W))*d;
 
-lst=find(mask(:,1));
-[r,p]=nirs.math.xcorrcoef(d(lst,:),maxlags,mask);
+warning('off','stats:statrobustfit:IterationLimit');
+
+for i = 1:N
+    for j = i:N
+        
+        lst=(mask(:,i) & mask(:,j));
+        tmp_d = d(lst,[i j]);
+        tmp_M = size(tmp_d,1);
+        tmp_inds = 1:tmp_M;
+        
+        for k = 1:numlag
+                        
+            lag = lags(k);
+            iinds = tmp_inds;
+            oinds = tmp_inds - lag;
+            dropinds = oinds<1 | oinds>tmp_M;
+            iinds(dropinds)=[];
+            oinds(dropinds)=[];
+            
+            d1 = tmp_d(:,1);
+            d2 = zeros(tmp_M,1);
+            d2(oinds) = tmp_d(iinds,2);
+            
+            b1 = robustfit(d1,d2,'bisquare',[],'on');
+            b2 = robustfit(d2,d1,'bisquare',[],'on');
+            
+            val = sign(b1(2)+b2(2)) * sqrt(abs(b1(2).*b2(2)));
+            
+            r(i,j,numlag-k+1) = val;
+            r(j,i,k) = val;
+        end
+    end
+end
+
+n=sum(mask(:,1));
+
+Tstat = r .* sqrt((n-2) ./ (1 - r.^2));
+p = 2*nirs.math.tpvalue(-abs(Tstat),n-2);
+p=p+repmat(eye(N),[1 1 numlag]);
 
 end
 
@@ -36,4 +76,3 @@ r = r/s/4.685;
 
 w = (1 - r.^2) .* (r < 1 & r > -1);
 end
-
--- +nirs/+math/xcorrcoef.m
+++ +nirs/+math/xcorrcoef.m
@@ -1,73 +1,76 @@
-function [r,p]=xcorrcoef(d,maxlags,mask)
-% This returns the largest magnitude cross-correlation coefficient
-% [r,p]=nirs.math.xcorrcoef(d,maxlags,mask)
+function [r,p] = xcorrcoef( d , maxlags , verbose , mask )
 
 if(nargin<2 || isempty(maxlags))
     maxlags=size(d,1);
 end
 
-if(ischar(maxlags))
-    maxlags = Fs*str2double(maxlags(1:strfind(maxlags,'x')-1));
+if(nargin<3 || isempty(verbose))
+    verbose=false;
 end
 
-if(nargin<3)
+if(nargin<4)
     mask=ones(size(d));
 end
 
-% all pairwise combinations
-r=ones(size(d,2));
-n=size(d,2)^2;
+[M,N] = size(d);
+maxlags = ceil(maxlags);
+lags = -maxlags:maxlags;
+numlag = length(lags);
+r = zeros(N,N,numlag);
 
-cnt=1;
-for i=1:size(d,2)
-    for j=1:size(d,2)
-        lst=find(mask(:,i) & mask(:,j));
-        rs = xcorr(d(lst,i),d(lst,j),maxlags,'Coeff');
-        r(i,j)=rs(find(abs(rs)==max(abs(rs)),1));
-        cnt=cnt+1;
+d=bsxfun(@minus,d,mean(d,1));
+d=bsxfun(@rdivide,d,std(d,1,1));
+
+W=ones(size(d,1),1);
+for idx=1:size(d,2)
+    W = min(W,wfun(d(:,idx)));
+end
+% Weight the whole model first to down-weight global motion artifacts
+d=spdiags(W,0,length(W),length(W))*d;
+
+for i = 1:N
+    for j = i:N
+        
+        lst=(mask(:,i) & mask(:,j));
+        tmp_d = d(lst,[i j]);
+        tmp_M = size(tmp_d,1);
+        tmp_inds = 1:tmp_M;
+        
+        for k = 1:numlag
+                        
+            lag = lags(k);
+            iinds = tmp_inds;
+            oinds = tmp_inds - lag;
+            dropinds = oinds<1 | oinds>tmp_M;
+            iinds(dropinds)=[];
+            oinds(dropinds)=[];
+            
+            d1 = tmp_d(:,1);
+            d2 = zeros(tmp_M,1);
+            d2(oinds) = tmp_d(iinds,2);
+            
+            b1 = regress(d1,d2);
+            b2 = regress(d2,d1);
+            
+            val = sign(b1+b2) * sqrt(abs(b1.*b2));
+            
+            r(i,j,numlag-k+1) = val;
+            r(j,i,k) = val;
+        end
     end
 end
-r(r>1) = 1;
-r(r<-1) = -1;
 
-n=size(d,1);
+n=sum(mask(:,1));
 
 Tstat = r .* sqrt((n-2) ./ (1 - r.^2));
-p = 2*tpvalue(-abs(Tstat),n-2);
-p=tril(p,-1)+tril(p,-1)'+eye(size(p));
+p = 2*nirs.math.tpvalue(-abs(Tstat),n-2);
+p=p+repmat(eye(N),[1 1 numlag]);
 
 end
 
-function p = tpvalue(x,v)
-%TPVALUE Compute p-value for t statistic.
+function w = wfun(r)
+s = mad(r, 0) / 0.6745;
+r = r/s/4.685;
 
-normcutoff = 1e7;
-if length(x)~=1 && length(v)==1
-    v = repmat(v,size(x));
-end
-
-% Initialize P.
-p = NaN(size(x));
-nans = (isnan(x) | ~(0<v)); % v == NaN ==> (0<v) == false
-
-% First compute F(-|x|).
-%
-% Cauchy distribution.  See Devroye pages 29 and 450.
-cauchy = (v == 1);
-p(cauchy) = .5 + atan(x(cauchy))/pi;
-
-% Normal Approximation.
-normal = (v > normcutoff);
-p(normal) = 0.5 * erfc(-x(normal) ./ sqrt(2));
-
-% See Abramowitz and Stegun, formulas 26.5.27 and 26.7.1.
-gen = ~(cauchy | normal | nans);
-p(gen) = betainc(v(gen) ./ (v(gen) + x(gen).^2), v(gen)/2, 0.5)/2;
-
-% Adjust for x>0.  Right now p<0.5, so this is numerically safe.
-reflect = gen & (x > 0);
-p(reflect) = 1 - p(reflect);
-
-% Make the result exact for the median.
-p(x == 0 & ~nans) = 0.5;
+w = (1 - r.^2) .* (r < 1 & r > -1);
 end
