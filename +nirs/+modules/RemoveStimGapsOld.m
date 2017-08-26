6f113bb2178bfbb7c5dc589c29c1620ed9e1df61
Frank Fishburn <fishburnf@upmc.edu>
2017-08-26 14:44:02 -0400

    Add the old method for removing stim gaps

---
+++ +nirs/+modules/RemoveStimGapsOld.m
@@ -0,0 +1,56 @@
+classdef RemoveStimGapsOld < nirs.modules.AbstractModule
+%% RemoveStimGaps - Merges task blocks that are only separated by a user-specified interval.
+% 
+% Options: 
+%     max_gap_length - scalar maximum duration (in samples) of gap to remove
+
+    properties
+        max_gap_length = 1; % maximum duration of gap to remove
+    end
+    
+    methods
+        function obj = RemoveStimGapsOld( prevJob )
+         	if nargin > 0, obj.prevJob = prevJob; end
+            obj.name = 'Remove gaps in stim block';
+        end
+        
+        function data = runThis( obj, data )
+            
+            assert(isa(data,'nirs.core.Data'));
+            
+            for i = 1:length(data)
+                stimNames = nirs.getStimNames(data(i));
+                for j = 1:length(stimNames)
+                    
+                    stims = data(i).stimulus(stimNames{j});
+                    onset = stims.onset * data(i).Fs;
+                    dur = stims.dur * data(i).Fs;
+                    amp = stims.amp;
+                    offset = onset + dur;
+                    diff = onset(2:end) - offset(1:end-1);
+                    mergeInds = find(diff<=obj.max_gap_length);
+                    
+                    if ~isempty(mergeInds)
+                    
+                        for k = length(mergeInds):-1:1
+
+                            onset(mergeInds(k)+1) = [];
+                            offset(mergeInds(k)) = [];
+                            amp(mergeInds(k)+1) = [];
+
+                        end
+                        stims.onset = onset ./ data(i).Fs;
+                        stims.dur = (offset - onset) ./ data(i).Fs;
+                        stims.amp = amp;
+
+                        data(i).stimulus(stimNames{j}) = stims;
+                        
+                    end
+                end
+                
+            end            
+        end
+    end
+    
+end
+
