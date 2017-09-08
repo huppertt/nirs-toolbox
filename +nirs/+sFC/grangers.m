1496953487668174f49d7e5451df73fcdcee2a8c
Frank Fishburn <fishburnf@upmc.edu>
2017-09-08 14:32:37 -0400

    Update existing Granger code with corr_fcn flags, and add compatibility wrapper

--- +nirs/+sFC/grangers.m
+++ +nirs/+sFC/grangers.m
@@ -1,9 +1,35 @@
-function [G,F,dfe1,dfe2,p]=grangers(data)
+function [G,F,dfe1,dfe2,p]=grangers(data,modelorder,robust_flag,includeZeroLag)
 % This function runs the Granger's causality models
+if(nargin<4)
+    includeZeroLag=true;
+end
 
-%TODO- write this
+if(nargin<3)
+    robust_flag=true;
+end
+
+if(nargin<2 || isempty(modelorder))
+    modelorder=20;
+end
+
+if(~isempty(strfind(class(data),'.core.Data')))
+    Fs=data.Fs;
+    data=data.data;
+else
+    Fs=1;
+end
 
 
-[G, F, df1, df2, p] = nirs.math.mvgc(Y, Pmax,includeZeroLag);
+if(ischar(modelorder))
+    Pmax = Fs*str2double(modelorder(1:strfind(modelorder,'x')-1));
+else
+    Pmax = modelorder;
+end
 
-[G, F,dfe1,dfe2, P] = nirs.math.robust_mvgc(data(i).data,pMax,includeZeroLag);
+if robust_flag
+    [G, F, dfe1, dfe2, p] = nirs.math.robust_mvgc(data,Pmax,includeZeroLag);
+else
+    [G, F, dfe1, dfe2, p] = nirs.math.mvgc(data,Pmax,includeZeroLag);
+end
+
+end
---
+++ +nirs/+sFC/grangers_compat.m
@@ -0,0 +1,22 @@
+function [R,p,dfe]=grangers_compat(varargin)
+% [R,p,dfe]=grangers_compat(data,modelorder,robust_flag,includeZeroLag)
+%
+% This is a wrapper for the grangers code to maintain compatibility with
+%   the connectivity modules and sFC data class.
+% The F-stats are converted to R-values, which will likely need to be 
+%   centered by subtracting a null condition
+
+[G,F,dfe1,dfe2,p] = nirs.sFC.grangers(varargin{:});
+
+% Remove directionality
+F = max( F , permute(F,[2 1 3]) );
+
+% Convert to Fisher Z-distribution
+Z = log( F ) / 2;
+
+% Convert to r-value
+R = tanh(Z);
+
+dfe = nanmean(dfe2(:));
+
+end
