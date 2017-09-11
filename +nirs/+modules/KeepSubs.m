f35f143e64fa4a60abe670a5ea4afff84322bc58
Frank Fishburn <fishburnf@upmc.edu>
2017-09-11 13:35:12 -0400

    Extend KeepSubs to multiple allowable (eg. list of good subjects), and selection inversion

--- +nirs/+modules/KeepSubs.m
+++ +nirs/+modules/KeepSubs.m
@@ -3,9 +3,11 @@ classdef KeepSubs < nirs.modules.AbstractModule
     %
     % Options:
     %     listOfFilters - [N x 2] cell array of demographics key-value pairs
-    %                     to keep (e.g. {'Group', 'Control'; 'Gender', 'M' }
+    %                     to keep (e.g. {'Group', 'Control'; 'Gender', 'M'; 'SubjectID', {'Sub01','Sub03','Sub09'} }
+    %     remove_matches - logical (true inverts selection criteria; default: false)
     properties
         listOfFilters = {}; % cell array of demographic criteria to match
+        remove_matches = false; % Inverts the selection criteria to remove those who match
     end
     
     methods
@@ -26,11 +28,17 @@ classdef KeepSubs < nirs.modules.AbstractModule
                 
                 for j = 1:size(obj.listOfFilters,1)
                 
-                    if ~strcmpi( demo( obj.listOfFilters{j,1} ) , obj.listOfFilters{j,2} )
-                        
+                    value = demo( obj.listOfFilters{j,1} );
+                    allowable = obj.listOfFilters{j,2};
+                    is_match = any(strcmpi( allowable , value  ));
+                    
+                    if ~obj.remove_matches && ~is_match
                         keepers(i) = false;
-                        
                     end
+                    if obj.remove_matches && is_match
+                        keepers(i) = false;
+                    end
+                    
                 end
             end
             
