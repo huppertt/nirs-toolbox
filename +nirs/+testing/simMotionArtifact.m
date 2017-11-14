36b36a253610567bd3b9e8e639181302e555767f
Frank Fishburn <fishburnf@upmc.edu>
2017-11-14 18:31:37 -0500

    Add nirs.testing.simMotionArtifact()

---
+++ +nirs/+testing/simMotionArtifact.m
@@ -0,0 +1,45 @@
+function data = simMotionArtifact( data , spikes_per_minute , shifts_per_minute )
+if ~exist('spikes_per_minute','var') || isempty(spikes_per_minute), spikes_per_minute = 2; end
+if ~exist('shifts_per_minute','var') || isempty(shifts_per_minute), shifts_per_minute = .5; end
+
+num_spikes = round( spikes_per_minute * (data.time(end)-data.time(1))/60 );
+num_shifts = round( shifts_per_minute * (data.time(end)-data.time(1))/60 );
+
+nsamp = length(data.time);
+
+spike_inds = randi([2 nsamp-1],[1 num_spikes]);
+shift_inds = randi([2 nsamp-1],[1 num_shifts]);
+
+spike_amp_Z = 6 * randn([1 num_spikes]);
+shift_amp_Z = 25 * randn([1 num_shifts]);
+
+means = mean(data.data);
+stds = std(data.data);
+
+for i = 1:num_spikes
+    
+    amp = abs(spike_amp_Z(i)) .* stds;
+    width = 1.9*abs(rand) + .1;
+    
+    t_peak = data.time(spike_inds(i));
+    t_start = t_peak - width;
+    t_end = t_peak + width;
+    t_inds = find( (data.time > t_start) & (data.time < t_end) );
+    
+    for j = 1:ceil(length(t_inds)/2)
+        
+        t = data.time(t_inds(j)) - t_start;
+        data.data(t_inds(j),:) = data.data(t_inds(j),:) + amp .^ (t/width);
+        data.data(t_inds(end-j+1),:) = data.data(t_inds(end-j+1),:) + amp .^ (t/width);
+    end
+    
+end
+
+for i = 1:num_shifts
+    
+    shift_amt = shift_amp_Z(i) .* stds;
+    data.data(shift_inds(i):end,:)  = bsxfun( @plus , data.data(shift_inds(i):end,:) , shift_amt );
+    
+end
+
+end
