78fd5b8f6a5912e6c7e9c619435f9fd8d102db3b
Frank Fishburn <fishburnf@upmc.edu>
2018-01-02 15:04:18 -0500

    Various fixes to nirs.util.plot_model, esp. for models generated outside toolbox

--- +nirs/+util/plot_model.m
+++ +nirs/+util/plot_model.m
@@ -12,7 +12,7 @@ switch class(stats)
         weights = weights .* (40/median(weights));
         [~,worder] = sort(weights,'descend');
         
-        Coefs = stats.CoefficientNames;
+        Coefs = stats.PredictorNames;
         
         for i = 1:length(Coefs)
             clear cond xvar;
@@ -28,19 +28,24 @@ switch class(stats)
                 continue;
             end
             if ~exist('cond','var')
-                cond = stats.Variables.cond{1};
+                if isfield(stats.Variables,'cond')
+                    cond = stats.Variables.cond{1};
+                    inds = strcmp( stats.Variables.cond , cond );
+                else
+                    inds = 1:stats.NumObservations;
+                end
             end
             yvar = stats.ResponseName;
             
-            inds = strcmp( stats.Variables.cond , cond );
             xdata = stats.Variables.(xvar)(inds);
             ydata = stats.Variables.(yvar)(inds);
             
             xdist = max(xdata)-min(xdata);
             xlim = [min(xdata)-2*xdist max(xdata)+2*xdist];
             xpts = linspace(xlim(1),xlim(2),100)';
+            predind = find(strcmp(stats.CoefficientNames,xvar),1);
             X = ones(length(xpts),length(beta));
-            X(:,i) = xpts;
+            X(:,predind) = xpts;
             
             [ypred,ci] = nirs.math.predci( X , beta , covb , stats.MSE , stats.DFE , .05 , false , false , true );
             
@@ -60,7 +65,11 @@ switch class(stats)
             set( ax , 'xlim' , figxlim );
             xlabel(ax,['\bf' strrep(xvar,'_',' ')]);
             ylabel(ax,'\bfActivation \rm(\beta)');
-            title(ax,['\bfActivation x ' strrep(xvar,'_',' ') sprintf('\n') '\rm' cond]);
+            if exist('cond','var')
+                title(ax,['\bfActivation x ' strrep(xvar,'_',' ') sprintf('\n') '\rm' cond]);
+            else
+                title(ax,['\bfActivation x ' strrep(xvar,'_',' ')]);
+            end
         end
         
         
