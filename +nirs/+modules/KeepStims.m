cb257c38f54aff307618b140a10c2fce8c76c81e
Frank Fishburn <fishburnf@upmc.edu>
2018-01-31 14:59:46 -0500

    Add optional support for regex in KeepStims module

--- +nirs/+modules/KeepStims.m
+++ +nirs/+modules/KeepStims.m
@@ -8,6 +8,11 @@ classdef KeepStims < nirs.modules.AbstractModule
         required = false; % logical scalar or array [1 x ncond] indicating
                           % whether stimuli are required (subjects missing them will be removed)
                           % default: false
+                          
+        regex  = false;   % logical dictating whether to match condition names based on a regular experession
+                          % Ex. '^[012]back$' would match 0back, 1back, 2back
+                          % Ex. ':01$' would keep anything ending in :01
+                          % default: false
     end
     
     methods
@@ -18,6 +23,12 @@ classdef KeepStims < nirs.modules.AbstractModule
         
         function data = runThis( obj, data )
             
+            if obj.regex
+                checkmatch = @(conds,reference) ~cellfun(@isempty,regexpi(conds,reference,'forcecelloutput'));
+            else
+                checkmatch = @(conds,reference) strcmpi(conds,reference);
+            end
+            
             obj.required = logical(obj.required);
             if numel(obj.required)==1
                 obj.required = repmat(obj.required,size(obj.listOfStims));
@@ -25,71 +36,71 @@ classdef KeepStims < nirs.modules.AbstractModule
             if length(obj.required) ~= length(obj.listOfStims)
                 error('List of stims and required paramters not compatible. Lengths: %i and %i',length(obj.required),length(obj.listOfStims));
             end
+            
             bad_subjects = false(size(data));
             
             for i = 1:numel(data)
                 
-                if(isa(data(i),'nirs.core.Data') | isa(data(i),'eeg.core.Data') | isa(data(i),'dtseries.core.Data'))
-                    stim = data(i).stimulus;
-                    
-                    % get values for stims we keep
-                    keys = obj.listOfStims;
-                    if(isempty(keys))
-                        data(i).stimulus=Dictionary;
-                        continue;
+                if(isa(data(i),'nirs.core.Data') || isa(data(i),'eeg.core.Data') || isa(data(i),'dtseries.core.Data'))
+                    conds=data(i).stimulus.keys;
+                    isgoodcond=false(size(conds));
+                    for j=1:length(obj.listOfStims)
+                        match = checkmatch(conds,obj.listOfStims{j});
+                        isgoodcond = isgoodcond | match;
+                        if obj.required(j) && ~any(match)
+                            bad_subjects(i)=true;
+                        end
                     end
                     
-                    vals = stim( keys );
-                    
-                    if ~iscell(vals)
-                        vals = {vals};
-                    end
-                    
-                    % lst of nonempty
-                    lst = ~cellfun(@isempty, vals);
-                    
-                    % new stimulus
-                    data(i).stimulus = Dictionary(keys(lst), vals(lst));
-                    
-                    if any( obj.required(:) & ~lst(:) )
-                        bad_subjects(i) = true;
+                    badconds = conds(~isgoodcond);
+                    for j=1:length(badconds)
+                        data(i).stimulus = data(i).stimulus.remove(badconds{j});
                     end
                     
                 elseif(isa(data(i),'nirs.core.sFCStats'))
-                    cond=data(i).conditions;
-                    lst=find(ismember(cond,obj.listOfStims));
-                    data(i).R=data(i).R(:,:,lst);
-                    data(i).dfe=data(i).dfe(lst);
-                    data(i).conditions={data(i).conditions{lst}};
-                    if any(~ismember(obj.listOfStims(obj.required),cond))
-                        bad_subjects(i) = 1;
+                    conds=data(i).conditions;
+                    isgoodcond=false(size(conds));
+                    for j=1:length(obj.listOfStims)
+                        match = checkmatch(conds,obj.listOfStims{j});
+                        isgoodcond = isgoodcond | match;
+                        if obj.required(j) && ~any(match)
+                            bad_subjects(i)=true;
+                        end
                     end
+                    data(i).R=data(i).R(:,:,isgoodcond);
+                    data(i).dfe=data(i).dfe(isgoodcond);
+                    data(i).conditions={data(i).conditions{isgoodcond}};
                     
                 elseif(isa(data(i),'nirs.core.ChannelStats'))
                     conds=data(i).variables.cond;
-                    isgood=false(size(conds));
+                    isgoodcond=false(size(conds));
                     for j=1:length(obj.listOfStims)
-                        isgood = isgood | strcmp(conds,obj.listOfStims{j});
+                        match = checkmatch(conds,obj.listOfStims{j});
+                        isgoodcond = isgoodcond | match;
+                        if obj.required(j) && ~match
+                            bad_subjects(i)=true;
+                        end
                     end
-                    data(i).variables(~isgood,:)=[];
-                    data(i).beta(~isgood) = [];
-                    data(i).covb(~isgood,:) = [];
-                    data(i).covb(:,~isgood) = [];
-                    if any(~ismember(obj.listOfStims(obj.required),conds(isgood)))
-                        bad_subjects(i) = 1;
-                    end
+                    data(i).variables(~isgoodcond,:)=[];
+                    data(i).beta(~isgoodcond) = [];
+                    data(i).covb(~isgoodcond,:) = [];
+                    data(i).covb(:,~isgoodcond) = [];
                     
                 elseif(isa(data(i),'nirs.core.ChannelFStats'))
                     conds=data(i).variables.cond;
-                    isgood=false(size(conds));
+                    isgoodcond=false(size(conds));
                     for j=1:length(obj.listOfStims)
-                        isgood = isgood | strcmp(conds,obj.listOfStims{j});
+                        match = checkmatch(conds,obj.listOfStims{j});
+                        isgoodcond = isgoodcond | match;
+                        if obj.required(j) && ~match
+                            bad_subjects(i)=true;
+                        end
                     end
-                    data(i).variables(~isgood,:)=[];
-                    data(i).F(~isgood) = [];
-                    data(i).df1(~isgood) = [];
-                    data(i).df2(~isgood) = [];
-                    if any(~ismember(obj.listOfStims(obj.required),conds(isgood)))
+                    data(i).variables(~isgoodcond,:)=[];
+                    data(i).F(~isgoodcond) = [];
+                    data(i).df1(~isgoodcond) = [];
+                    data(i).df2(~isgoodcond) = [];
+                    if any(~ismember(obj.listOfStims(obj.required),conds(isgoodcond)))
                         bad_subjects(i) = 1;
                     end
                     
