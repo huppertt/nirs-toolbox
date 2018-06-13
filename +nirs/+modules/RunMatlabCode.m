82d7ebe38768c0e19e027ae3c22bd28bff6d6a39
Xuetong Zhai <xuetong.zhai@gmail.com>
2018-06-13 16:28:52 -0400

    run group flag

--- +nirs/+modules/RunMatlabCode.m
+++ +nirs/+modules/RunMatlabCode.m
@@ -1,24 +1,24 @@
 classdef RunMatlabCode < nirs.modules.AbstractModule
-%%  RunMatlabCode - This is a generic wrapper function to run an feval statement 
-% on a data file as part of the pipeline 
-% 
-% Options: 
-%     FunctionHandle - a filename or function handle to call
-%                    must be of the form Data = fcn(Data)
-
+    %%  RunMatlabCode - This is a generic wrapper function to run an feval statement
+    % on a data file as part of the pipeline
+    %
+    % Options:
+    %     FunctionHandle - a filename or function handle to call
+    %                    must be of the form Data = fcn(Data)
+    
     properties
         FunctionHandle   % filename or function handle to call
-
+        rungroup;
     end
     
     methods
-
+        
         function obj =  RunMatlabCode( prevJob )
-           obj.name = 'Pass data into generic matlab function';
-           
-           if nargin > 0
-               obj.prevJob = prevJob;
-           end
+            obj.name = 'Pass data into generic matlab function';
+            obj.rungroup=false;
+            if nargin > 0
+                obj.prevJob = prevJob;
+            end
         end
         
         function data = runThis( obj, data )
@@ -26,11 +26,15 @@ classdef RunMatlabCode < nirs.modules.AbstractModule
             if(nargin(obj.FunctionHandle)==0)
                 feval(obj.FunctionHandle);
             else
-                for i = 1:numel(data)
-                    data(i)=feval(obj.FunctionHandle,data(i));
+                if(~obj.rungroup)
+                    for i = 1:numel(data)
+                        data(i)=feval(obj.FunctionHandle,data(i));
+                    end
+                else
+                    data=feval(obj.FunctionHandle,data);
                 end
             end
         end
-    
+        
     end
 end
