afe2b5edc2cdd2517445d778436359e8b50f0129
Frank Fishburn <fishburnf@upmc.edu>
2018-07-11 22:32:44 -0400

    Add simulated physiological noise to hb concentration

--- +nirs/+testing/simPhysioNoise.m
+++ +nirs/+testing/simPhysioNoise.m
@@ -3,40 +3,104 @@ function [data, truth] = simPhysioNoise( data , cardiac_amp , resp_amp , mayer_a
 if nargin<1 || ~exist('data','var') || isempty(data)
    [data, truth]=nirs.testing.simData; 
 end
-if nargin<2 || isempty(cariac_amp)
-    cardiac_amp = .4;
+if nargin<2 || isempty(cardiac_amp)
+    cardiac_amp = .25;
 end
 if nargin<3 || isempty(resp_amp)
-    resp_amp = .3;
+    resp_amp = .25;
 end
 if nargin<3 || isempty(mayer_amp)
-    mayer_amp = .2;
+    mayer_amp = .25;
 end
 
-nsamp = length(data.time);
 time = data.time;
 [nsamp,nchan] = size(data.data);
 
 % Generate cardiac oscillations
 cardiac_freq = 1 + .1*randn;
 cardiac_phase = cumsum( .01*2*pi*randn(nsamp,1) , 1 );
-cardiac_amp = cardiac_amp * std(data.data,0,1);
-cardiac_data = cardiac_amp .* sin( 2*pi*cardiac_freq*time + cardiac_phase );
+cardiac_data = sin( 2*pi*cardiac_freq*time + cardiac_phase );
 
 % Generate respiratory oscillations
 resp_freq = .25 + .025*randn;
 resp_phase = cumsum( .01*2*pi*randn(nsamp,1) , 1 );
-resp_amp = resp_amp * std(data.data,0,1);
-resp_data = resp_amp .* sin( 2*pi*resp_freq*time + resp_phase );
+resp_data = sin( 2*pi*resp_freq*time + resp_phase );
 
 % Generate Mayer waves
 mayer_freq = .1 + .01*randn;
 mayer_phase = cumsum( .01*2*pi*randn(nsamp,1) , 1 );
-mayer_amp = mayer_amp * std(data.data,0,1);
-mayer_data = mayer_amp .* sin( 2*pi*mayer_freq*time + mayer_phase );
+mayer_data = sin( 2*pi*mayer_freq*time + mayer_phase );
 
-% Add physiological noises
-data.data = data.data + cardiac_data + resp_data + mayer_data;
+% Add noise to hb concentrations
+link = data.probe.link;
+Y    = data.data;
+
+if(~(iscellstr(link.type) && any(ismember(link.type,{'hbo','hbr'}))))
+
+    % optical density
+    m = mean(Y);
+    Y = bsxfun(@plus, -log(Y), log(m));
+    
+    % sort channels
+    channels = nirs.util.uniquerows(link(:,1:2));    
+    
+    for j = 1:height(channels)
+        
+        lst = find(link.source==channels.source(j) & link.detector==channels.detector(j));
+        
+        assert( length(lst) > 1 )
+
+        lambda = link.type(lst);
+        ext = nirs.media.getspectra( lambda );
+
+        clist = [1 2]; % hbo and hbr; need to fix this
+
+        % extinction coefficients
+        E = ext(:,clist);
+        
+        % sd distance
+        L = data.probe.distances(lst);
+        L=max(L,1);  % avoid issues with the short (0) seperation values
+
+        % mbll model
+        PPF = .1;
+        EL = bsxfun( @times, E, L*PPF );
+        iEL = pinv(EL);
+
+        % calculates chromophore concentration (uM)
+        hb = (Y(:,lst)*iEL') * 1e6;
+        
+        % add physiological noises
+        for k = 1:size(hb,2)
+            
+            sigma = std(hb(:,k),0,1);
+            hb(:,k) = hb(:,k) + cardiac_amp * sigma * cardiac_data ...
+                              + resp_amp * sigma * resp_data ...
+                              + mayer_amp * sigma * mayer_data;
+        end
+        
+        % Convert back to OD
+        Y(:,lst) = hb./1e6 * EL';
+        
+    end
+    
+    % Convert OD to intensity
+    Y = exp( -bsxfun(@minus, Y, log(m)) );
+    
+else
+    
+    % add physiological noise
+    for k = 1:size(Y,2)
+
+        sigma = std(Y(:,k),0,1);
+        Y(:,k) = Y(:,k) + cardiac_amp * sigma * cardiac_data ...
+                        + resp_amp * sigma * resp_data ...
+                        + mayer_amp * sigma * mayer_data;
+    end
+
+end
+
+data.data = Y;
 
 if(nargout>1)
     if(~exist('truth','var'))
