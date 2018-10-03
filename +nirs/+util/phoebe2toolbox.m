2c92a90d9457d2d64357ef3d350f45f063409909
huppertt <huppertt@localhost>
2018-10-03 12:02:34 -0400

    added nirs.util.<> code for reading and writing probes compatible with Phoebe

---
+++ +nirs/+util/phoebe2toolbox.m
@@ -0,0 +1,28 @@
+function probe1020=phoebe2toolbox(filename)
+
+filename=strtok(filename,'.');
+load([filename '.SD'],'-MAT','SD');
+
+
+probe=nirs.util.sd2probe(SD);
+
+% load the atlas model from Phoebe
+load('scalp_model');
+mesh=nirs.core.Mesh(vertices(:,2:4),faces(:,2:4));
+
+fid=table({'nas';'rpa';'lpa'},fiducials(:,1),fiducials(:,2),fiducials(:,3),repmat(cellstr('10-20'),3,1),repmat(cellstr('mm'),3,1),...
+    'VariableNames',{'Name','X','Y','Z','Type','Units'}); 
+mesh=mesh.addfiducials(fid);
+
+probe1020=nirs.core.Probe1020;
+probe1020.optodes_registered=probe.optodes;
+probe1020.link=probe.link;
+probe1020=probe1020.register_mesh2probe(mesh,false);
+
+XYZ=[probe.optodes.X probe.optodes.Y probe.optodes.Z];
+probe1020.optodes=probe.optodes;
+
+[probe1020.optodes.X,probe1020.optodes.Y]=probe1020.convert2d(XYZ);
+probe1020.optodes.Z(:)=0;
+
+
