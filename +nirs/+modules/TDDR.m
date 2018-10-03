95cac88dd68add7d2e2b73ffe2e45181c889144e
Frank Fishburn <fishburnf@upmc.edu>
2018-10-03 11:31:29 -0400

    Add new TDDR motion correction, remove obsolete TDDC method

---
+++ +nirs/+modules/TDDR.m
@@ -0,0 +1,26 @@
+classdef TDDR < nirs.modules.AbstractModule
+%% Temporal Derivative Distribution Repair - Corrects motion artifacts by downweighting outlier fluctuations
+    methods
+        function obj = TDDR( prevJob )
+           obj.name = 'Temporal Derivative Distribution Repair';
+           obj.citation=['Fishburn, Frank A., Ludlum, Ruth S., Vaidya, Chandan J., and Medvedev, Andrei V. '...
+                '"Temporal Derivative Distribution Repair (TDDR): A motion correction method for fNIRS." '...
+                'NeuroImage 184 (2019): 171-179.'];
+           if nargin > 0
+               obj.prevJob = prevJob;
+           end
+        end
+        
+        function data = runThis( obj, data )
+            
+            for i = 1:numel(data)
+            
+                data(i).data = nirs.math.tddr( data(i).data , data(i).Fs );
+
+            end
+            
+        end
+        
+    end
+    
+end
