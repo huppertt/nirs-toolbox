48ac434379ad0c3f08f0aaa134f5b7a5e836a75e
huppertt <huppertt@localhost>
2019-01-18 12:19:01 -0500

    added flag top roi laterality

--- +nirs/+util/roilaterality.m
+++ +nirs/+util/roilaterality.m
@@ -1,9 +1,19 @@
-function tbl = roilaterality(Stats,ROI1,ROI2)
+function tbl = roilaterality(Stats,ROI1,ROI2,flag)
 % computes a laterality index defined as
 % LI = (ROI1-ROI2)/(ROI1+ROI2)
 
+if(nargin<4)
+    flag=false;
+end
+
 Diff = nirs.util.roi_math(ROI1,'-',ROI2);
 Sum = nirs.util.roi_math(ROI1,'+',ROI2);
+
+if(flag)
+    for(i=1:length(Stats))
+        Stats(i).beta=abs(Stats(i).beta);
+    end
+end
 
 tblD = nirs.util.roiAverage(Stats,Diff);
 tblS = nirs.util.roiAverage(Stats,Sum);
