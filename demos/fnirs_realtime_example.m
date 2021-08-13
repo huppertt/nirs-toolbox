c34534886c2c032192e8fb28c8ecd9d182a73eee
Theodore Huppert <huppert1@pitt.edu>
2021-08-13 12:57:54 -0400

    Added new realtime example

---
+++ demos/fnirs_realtime_example.m
@@ -0,0 +1,48 @@
+%This is a demo of how to use the new (in progress) real-time library
+
+% create an instance of the data storage class
+data = nirs.realtime.core.Data;
+
+% create a realtime analysis pipeline
+job = nirs.realtime.modules.BandPass;
+job = nirs.realtime.modules.MotionCorrect(job);
+
+% assign the pipeline to the data such that the pipeline is called
+% everytime the data is updated
+data.updatefunction=job;
+
+% we could update the data manually using
+% >> data.updatedata(randn(1,numchan),time)
+
+listener = nirs.realtime.listeners.simulator;
+listener.datasource = nirs.testing.simData;  % where to get the simulation data from
+listener.data_output = data;  % where to send the data (this is the storage class we just created above)
+
+listener.start;
+data.draw;  % now that the storage class is getting data, we can draw it
+
+listener.stop;
+
+
+
+
+% LSL example
+
+data = nirs.realtime.core.Data;
+
+% create a realtime analysis pipeline
+job = nirs.realtime.modules.BandPass;
+job = nirs.realtime.modules.MotionCorrect(job);
+data.updatefunction=job;
+
+
+listener = nirs.realtime.listeners.lslStream;
+listener.LSLdata_StreamName='NIRx';
+listener.LSLmarker_StreamName='EprimeLSL';
+listener.data_output = data;  % where to send the data (this is the storage class we just created above)
+
+listener.start;
+
+data.draw;  % now that the storage class is getting data, we can draw it
+
+listener.stop;
