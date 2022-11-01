5454d0c5022b22e729daffbb3df5478340be40de
Adrian Curtin <48138055+AdrianCurtin@users.noreply.github.com>
2022-11-01 16:37:15 -0400

    Update change_stimulus_duration.m

--- +nirs/+design/change_stimulus_duration.m
+++ +nirs/+design/change_stimulus_duration.m
@@ -1,5 +1,17 @@
 function data=change_stimulus_duration(data,stimname,duration)
 % This function will change the duration of a stimulus in a data variable
+%   If stimname is empty, duration for all stimuli will be changed
+%       ex: change_stimulus_duration(data,[],10)
+%           sets stimuli in data ('A' and 'B') to 10
+%       ex: change_stimulus_duration(data,{'A'},10)
+%           sets stimulu in data ('A') to 10, leaves B
+%
+%   If an array of data objects is passed through, stimulus length is
+%       changed for each object in data
+
+if(nargin<3)
+    error('New stimulus duration must be provided');
+end
 
 
 if(length(data)>1)
