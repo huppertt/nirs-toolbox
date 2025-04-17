1e122a8c178214dfc1e5765e75ece166c3a59af2
huppertt <huppert1@pitt.edu>
2025-04-17 16:38:49 -0400

    Fixed copy/paste typos in SCI code

--- +nirs/+util/scalp_coupling_index.m
+++ +nirs/+util/scalp_coupling_index.m
@@ -38,18 +38,20 @@ for i=1:height(link)
     nirs_data2=data.data(:,lst(2));
 
      
-    %Filter everything but t if(~legacy)
+    %Filter everything but the cardiac component
+    filtered_nirs_data1=filtfilt(B1,A1,nirs_data1);       % Cardiac bandwidth
+    filtered_nirs_data2=filtfilt(B1,A1,nirs_data2);       % Cardiac bandwidth
+   
+    if(~legacy)
         st=round(length(filtered_nirs_data1)*.05);
         ed=round(length(filtered_nirs_data1)*.95);
         filtered_nirs_data1=filtered_nirs_data1(st:ed);
         filtered_nirs_data2=filtered_nirs_data2(st:ed);
-    endhe cardiac component
-    filtered_nirs_data1=filtfilt(B1,A1,nirs_data1);       % Cardiac bandwidth
+    end
+   
     filtered_nirs_data1=filtered_nirs_data1./repmat(std(filtered_nirs_data1,0,1),size(filtered_nirs_data1,1),1); % Normalized heartbeat
-    filtered_nirs_data2=filtfilt(B1,A1,nirs_data2);       % Cardiac bandwidth
     filtered_nirs_data2=filtered_nirs_data2./repmat(std(filtered_nirs_data2,0,1),size(filtered_nirs_data2,1),1); % Normalized heartbeat
 
-   
 
     similarity = xcorr(filtered_nirs_data1,filtered_nirs_data2,'unbiased');  %cross-correlate the two wavelength signals - both should have cardiac pulsations
     similarity = length(filtered_nirs_data1)*similarity./sqrt(sum(abs(filtered_nirs_data1).^2)*sum(abs(filtered_nirs_data2).^2));  % this makes the SCI=1 at lag zero when x1=x2 AND makes the power estimate independent of signal length, amplitude and Fs
